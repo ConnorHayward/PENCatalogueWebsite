1a82aaff12e454964d4ab1fe267bf7363c3220fa
ConnorHayward <connor.hayward@hotmail.co.uk>
2018-09-19 15:14:00 +0200

    Update for julia 1.0

--- src/PENCatalogueManager.jl
+++ src/PENCatalogueManager.jl
@@ -4,17 +4,17 @@ module PENCatalogueManager
 
 ### Check for required modules.
 
-if !isdir(Pkg.dir("Glob"))
-    Pkg.add("Glob")
-end
-
-if !isdir(Pkg.dir("JSON"))
-    Pkg.add("JSON")
-end
-
-if !isdir(Pkg.dir("FileIO"))
-    Pkg.add("FileIO")
-end
+# if !isdir(Pkg.dir("Glob"))
+# Pkg.add("Glob")
+# end
+#
+# if !isdir(Pkg.dir("JSON"))
+# Pkg.add("JSON")
+# end
+#
+# if !isdir(Pkg.dir("FileIO"))
+# Pkg.add("FileIO")
+# end
 
 ### Creating files for batches and tiles####
 using FileIO
