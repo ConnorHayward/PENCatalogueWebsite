e378610a6321d6f15893c9a78ad8d86972c92755
ConnorHayward <connor.hayward@hotmail.co.uk>
2019-02-07 11:03:11 +0100

    Update to home page

--- PEN_Cat_Website/app/resources/home_page/HomeController.jl
+++ PEN_Cat_Website/app/resources/home_page/HomeController.jl
@@ -1,8 +1,14 @@
 module HomeController
 using Genie.Renderer
 
+cur_target = 0;
+
 function home()
-  html!(:home_page, :home_page)
+  html!(:home_page, :home_page,voltage=cur_target)
+end
+
+function set_values()
+    cur_target = (@params(:pmt_voltage));
 end
 
 end
